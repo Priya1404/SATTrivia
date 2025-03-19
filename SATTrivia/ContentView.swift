f779749ac33fcee2567d12e7dcc63cd16067971f
Priya Srivastava <priya140409@gmail.com>
2025-03-19 11:40:37 +0530

    [X]: project initialised

---
+++ SATTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SATTrivia
+//
+//  Created by Priya Srivastava on 19/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SATTrivia/SATTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SATTriviaApp.swift
+//  SATTrivia
+//
+//  Created by Priya Srivastava on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SATTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
